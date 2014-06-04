7c8ffe62f383e3fc1c0bcf0e642edc4431f5dacf
Matt Kemp <matt@mattikus.com>
2014-06-04 01:07:55 -0500

    Added in colleges for players. Add that information to the JSON output.

--- lib/player.rb
+++ lib/player.rb
@@ -5,10 +5,11 @@ class Player
   def initialize(name, college="")
     @name = name
     @id = ::Digest::MD5::hexdigest(@name)
+    @college = college
   end
 
   def to_h
-    { name: @name, id: @id }
+    { name: @name, id: @id, college: @college }
   end
 
   def to_json(*a)
